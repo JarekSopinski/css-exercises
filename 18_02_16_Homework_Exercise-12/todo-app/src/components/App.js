00ebd29007350c21848c16f1df39cc455fb61138
JarekSopinski <jarek.sopinski@gmail.com>
2018-02-16 17:13:52 +0100

    added and rendered todos list

--- 18_02_16_Homework_Exercise-12/todo-app/src/components/App.js
+++ 18_02_16_Homework_Exercise-12/todo-app/src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Todo from './Todo';
-import AddTodo from './AddTodo';
 
 export default class extends React.Component {
     render() {
         return <React.Fragment>
             <Todo/>
-            <AddTodo/>
         </React.Fragment>
     }
 }
--- 18_02_16_Homework_Exercise-12/todo-app/src/components/Todo.js
+++ 18_02_16_Homework_Exercise-12/todo-app/src/components/Todo.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 export default class extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [
+                {name: 'Repair R2-D2', isFinished: true},
+                {name: 'Find Obi-wan Kenobi', isFinished: true},
+                {name: 'Leave Tatooine', isFinished: true},
+                {name: 'Save princess Leina', isFinished: false},
+                {name: 'Escape from the Death Star', isFinished: false},
+                {name: 'Blow up the Death Star', isFinished: false}
+            ]
+        }
+    }
+
     render() {
         return <React.Fragment>
-            <p>Todo</p>
+            {this.state.todos.map((todo, id) =>
+            <li key={id}>{todo.name}</li>
+            )}
         </React.Fragment>
     }
 }
