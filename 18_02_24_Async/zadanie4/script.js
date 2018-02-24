9b3a07ab6bdd488e024adf06a2631bfcd3a6730a
JarekSopinski <jarek.sopinski@gmail.com>
2018-02-24 13:01:23 +0100

    Async task4

--- 18_02_24_Async/zadanie4/script.js
+++ 18_02_24_Async/zadanie4/script.js
@@ -9,6 +9,71 @@
  Dane do body zbierz z formularza. Dane wyślij dodając event click na submit button.
  */
 
+const $submitBtn = $('#submit');
+const $questionBody = $('#question-body');
+const $questionLevel = $('#question-level');
+const $questionCategory = $('#question-category');
+const $postMessage = $('#post-message');
+
+const url = 'https://api.fefaq.pl/questions';
+
+
+$submitBtn.on('click', function (e) {
+
+    e.preventDefault();
+
+    const question = $questionBody.val();
+    const level = $questionLevel.val();
+    const category = $questionCategory.val();
+
+    const newQuestion = {
+        question: question,
+        level: level,
+        category: category
+    };
+
+    console.log(newQuestion);
+
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(newQuestion),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+        .then(res => res.json())
+        .catch(error => $postMessage.text('Nie udało się wysłać pytania!'))
+        .then(response => $postMessage.text('Pomyślnie wysłano pytanie'));
+
+});
+
+
+/*
+const postQuestion = (body) => {
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body)
+    })
+};
+
+const sendQuestion = (e) => {
+    e.preventDefault();
+
+    body = {
+        question: $questionBody.value,
+        level: $questionLevel.value,
+        category: $questionCategory.value
+    };
+
+    postQuestion(body)
+        .then(res => res.json())
+        .catch(error => $postMessage.text('Nie udało się wysłać pytania!'))
+        .then(response => $postMessage.text('Pomyślnie wysłano pytanie'));
+};
+
+$submitBtn.addEventListener('click', sendQuestion);
+ */
+
 /* TODO:4.b jeśli pomyślnie dodałeś pytanie, wyświetl paragraf, że pomyślnie wysłano pytanie */
 /* TODO:4.c powyższy paragraf ma zniknąć po 3 sekundach */
 /* TODO:4.d obsłuż wyjątek - jeśli otrzymasz error, pokaż console.log z jakąś wiadomością */
